c4933fb414ab12434a2ee542e6be99bd583443d7
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-02-12 20:31:46 +0100

    new

--- src/pages/welcomePage.jsx
+++ src/pages/welcomePage.jsx
@@ -1,7 +1,7 @@
 export const Welcome = () => {
     return (
         <>
-            <div className="bg-deepBlue h-screen text-white flex justify-center items-center">
+            <div className="bg-[#003366] h-screen text-white flex justify-center items-center">
                 <h1 className=" p-6 text-black ">AnonTruth</h1>
             </div>
         </>
