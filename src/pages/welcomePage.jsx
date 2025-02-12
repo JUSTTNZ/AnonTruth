c89b1d039bf5011c3ec998330414f8b0e295e041
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-02-12 22:35:16 +0100

    .

--- src/pages/welcomePage.jsx
+++ src/pages/welcomePage.jsx
@@ -1,8 +1,20 @@
 export const Welcome = () => {
     return (
         <>
-            <div className="bg-deepBlue h-screen text-white flex justify-center items-center">
-                <h1 className=" p-6 text-black ">AnonTruth</h1>
+            <div className="bg-[#000] h-screen flex flex-col lg:py-[5%] sm:py-[50px] flex-wrap items-center">
+                <h1 className="text-[#00CCFF] font-bold text-3xl ">AnonTruth</h1>
+
+                <div>
+
+                </div>
+                <div>
+                    <h1 className="text-[#00CCFF] font-bold">Welcome</h1>
+                    <p className="text-[#00CCFF] font-bold">Behind the veil of anonymity, every whisper becomes a voice, every thought finds a space, say it all, no names, no limits</p>
+                </div>
+
+                <div>
+                    <p className="bg-[#00CCFF] font-bold text-lg text-white px-6 py-2 rounded-full">Getting started</p>
+                </div>
             </div>
         </>
     )
