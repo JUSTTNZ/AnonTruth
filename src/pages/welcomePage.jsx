431b0b216532e3cd0546b51fd8c578ad76b52909
CHUKWUDI CONFIDENCE OSINACHI <chukwudiconfidenceosinachi@gmail.com>
2025-02-14 06:47:46 +0100

    Update welcomePage.jsx

--- src/pages/welcomePage.jsx
+++ src/pages/welcomePage.jsx
@@ -27,7 +27,7 @@ export const Welcome = () => {
             <div className="bg-gray-900 h-screen flex flex-col justify-center flex-wrap items-center">
                 <h1 className="text-[#00CCFF] font-bold text-4xl md:text-6xl pb-4 md:pb-8">AnonTruth</h1>
 
-                <div className='pb-4 mb-6 flex flex-col items-center'>
+                <div className='pb-4 mb-10 flex flex-col items-center'>
                     <img className='w-24 h-24 md:w-60 md:h-60 mb-6  items-center' src={AnonImage2} alt="AnonImage" />
                     <h1 className="text-[#00CCFF] font-bold text-center text-3xl md:text-6xl pb-6 ">Welcome</h1>
                     <p className="text-[#00CCFF] font-normal text-sm px-14 md:text-xl text-center">Behind the veil of anonymity, every whisper becomes a voice, every thought finds a space, say it all, no names, no limits</p>
